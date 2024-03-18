ce3db4be5199f43686e2dc0dbc782a32edab9174
Emanuele Artegiani <emanuele.artegiani@studio.unibo.it>
2024-03-18 21:54:45 +0100

    Add test for Pell numbers stream

--- src/test/scala/u03/StreamTest.scala
+++ src/test/scala/u03/StreamTest.scala
@@ -1,4 +1,5 @@
 package u03
+
 import org.junit.*
 import org.junit.Assert.*
 import u03.Streams.*
@@ -10,7 +11,7 @@ import org.junit.jupiter.api.Assertions.assertAll
 
 class StreamTest:
 
-  @Test def testIterate(): Unit = 
+  @Test def testIterate(): Unit =
     val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
     assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Nil())))), toList(Stream.take(str1)(4)))
 
@@ -24,7 +25,7 @@ class StreamTest:
     val str2 = Stream.filter(str1)(x => x % 2 == 1) // {1,3,5,7,..}
     assertEquals(Cons(1, Cons(3, Cons(5, Cons(7, Nil())))), toList(Stream.take(str2)(4)))
 
-  @Test def takeWhile(): Unit = 
+  @Test def takeWhile(): Unit =
     val str1 = Stream.iterate(0)(_ + 1) // {0,1,2,3,..}
     val str2 = Stream.takeWhile(str1)(_ < 5) // {0,1,2,3,4}
     assertEquals(Cons(0, Cons(1, Cons(2, Cons(3, Cons(4, Nil()))))), Stream.toList(str2))
@@ -36,3 +37,6 @@ class StreamTest:
       () => assertEquals(Nil(), toList(str1)),
       () => assertEquals(Cons("x", Cons("x", Cons("x", Cons("x", Cons("x", Nil()))))), toList(str2))
     )
+
+  @Test def testPell(): Unit =
+    assertEquals(Cons(0, Cons(1, Cons(2, Cons(5, Cons(12, Nil()))))), Stream.toList(Stream.take(pell)(5)))
